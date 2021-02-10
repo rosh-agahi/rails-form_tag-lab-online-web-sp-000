9548bee073d8ca6a3c942f4942aff43006bfb2a6
Roshanak Agahi <rosh.agahi@gmail.com>
2021-02-10 06:45:27 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,9 @@ class StudentsController < ApplicationController
   def new 
   end
 
-  def create 
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
   end
 
 end
